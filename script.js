c7f40445e49f2fbe70950f300f18377c97ccc0cc
Aria Nur Jamal <jamal.psw@gmail.com>
2022-06-16 21:10:26 +0700

    slicing javascript code

---
+++ script.js
@@ -0,0 +1,5 @@
+// menambah event scroll untuk sticky nav
+window.addEventListener("scroll", function () {
+    let header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+  });
